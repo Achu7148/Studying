fef19b0a6d5a3e8daf0709f951575b58669d777a
Ashwanth <30797633+Achu7148@users.noreply.github.com>
2018-09-05 19:52:41 +0530

    Created ExcelReadColumnName program
    Created  a program for reading values from excel using column name, Wherever u place the column name with values, it gets extracted. Very useful

---
+++ src/main/java/Others/ReadColumnExcelWorking.java
@@ -0,0 +1,58 @@
+package Others;
+
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class ReadColumnExcelWorking {
+	public static String inputFile = "D:\\PegaTestData.xls";
+
+	@SuppressWarnings("unlikely-arg-type")
+	
+	
+	public static void readSheet(String strInputTestDataValue, String strSheetNo) throws IOException {
+		System.out.println("File repository is  : " + inputFile);
+		File inputWorkbook = new File(inputFile);
+		Workbook w;
+		try {
+			w = Workbook.getWorkbook(inputWorkbook);
+			// Get the sheet according to the sheetNo
+			Sheet sheet = w.getSheet(strSheetNo);
+			int intColumn = sheet.getColumns();
+			int intRows = sheet.getRows();
+			int tmpColumnNum = -1;
+			for (int columnCount = 0; columnCount < intColumn; columnCount++) {
+				for (int rowCount = 0; rowCount < intRows; rowCount++) {
+					Cell cell = sheet.getCell(columnCount, rowCount);
+					if (cell != null) {
+						// content = cell.getContents();
+						String cellContent = cell.getContents();
+						// if(strInputTestDataValue.equals(arg0))
+						if (tmpColumnNum >= 0 && columnCount == tmpColumnNum) {
+							System.out.println("Test Data Value is : " + cellContent);
+						}
+						if (cellContent.equalsIgnoreCase(strInputTestDataValue)) {
+							tmpColumnNum = columnCount;
+							// System.out.println("Test Data Value is : " + strInputTestDataValue);
+							// return strInputTestDataValue ;
+						}
+					}
+
+				}
+
+			}
+
+		} catch (BiffException e) {
+
+		}
+		// return null;
+	}
+
+	public static void main(String[] args) throws IOException {
+		readSheet("Test", "Sheet1");
+	}
+}
